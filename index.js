35b2a4dfcecd8cce8032f54a9c3e511844510e45
VSNSRUJANA123 <srujanadvsn@gmail.com>
2025-03-22 21:03:28 +0530

    backend

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors({}));
 const PORT = process.env.PORT || 5000;
 app.use("/sponsor", require("./routers/sponser"));
-app.use("/testitemdeatils", require("./routers/tableItem"));
+app.use("/testitemdeatils", require("./routers/testItemdetails"));
 app.use("/studytitle", require("./routers/studytitle"));
 app.get("/working", (req, res) => {
   return res.send({ message: "hi welcome to paint application" });
